75bbc8b1cc817b32cc9ba16a37b29c9e1bd049ad
bealeebrandt <70760839+bealeebrandt@users.noreply.github.com>
2020-12-11 10:29:02 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -37,9 +37,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    s = 'Have you eaten?'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=s))
 
 
 if __name__ == "__main__":
