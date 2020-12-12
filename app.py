47345b5fd4ec56ad961b56705ffc5f162f050e9a
bealeebrandt <70760839+bealeebrandt@users.noreply.github.com>
2020-12-12 09:47:34 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -38,7 +38,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = 'Have you eaten?'
+    r = 'Sorry! I do not understand what you are saying?!'
+    if msg == 'hi':
+        r = 'hi'
+    elif msg == 'have you eaten?'
+        r = 'not yet'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s))
